191fa47e99a1e44305b4e90b2c49502a2b800211
mahan-vzmz <mahan.varmazyar@gmail.com>
2026-05-31 02:35:06 +0330

    Update incidents.py

--- app/api/routes/incidents.py
+++ app/api/routes/incidents.py
@@ -1,26 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from app.schemas.ticket import (
-    SimilarTicketRequest
-)
-
-from app.services.incident_service import (
-    IncidentService
-)
+from app.schemas.ticket import SimilarTicketRequest
+from app.services.incident_service import IncidentService
 
 router = APIRouter()
-
 service = IncidentService()
 
 
 @router.post("/detect")
-async def detect_incident(
+async def detect_incident(request: SimilarTicketRequest):
 
-    request: SimilarTicketRequest
-):
+    try:
+        result = service.analyze_incident(
+            query=request.query
+        )
+        return result
 
-    result = service.analyze_incident(
-        query=request.query
-    )
-
-    return result
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
