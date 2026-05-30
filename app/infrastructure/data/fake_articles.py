ae9e06719345bffc3caa72965d3ef12481db857d
mahan-vzmz <mahan.varmazyar@gmail.com>
2026-05-31 02:01:51 +0330

    Add files via upload

---
+++ app/infrastructure/data/fake_articles.py
@@ -0,0 +1,57 @@
+articles = [
+
+    {
+        "id": 1,
+
+        "title":
+            "VPN Troubleshooting Guide",
+
+        "content":
+            """
+            If VPN login fails,
+            verify MFA token,
+            internet connectivity
+            and VPN credentials.
+            """
+    },
+
+    {
+        "id": 2,
+
+        "title":
+            "MFA Reset Procedure",
+
+        "content":
+            """
+            Reset MFA from security portal
+            and enroll your device again.
+            """
+    },
+
+    {
+        "id": 3,
+
+        "title":
+            "Outlook Login Issues",
+
+        "content":
+            """
+            Verify Exchange connectivity,
+            mailbox status
+            and user credentials.
+            """
+    },
+
+    {
+        "id": 4,
+
+        "title":
+            "Printer Troubleshooting",
+
+        "content":
+            """
+            Restart print spooler service
+            and verify printer connectivity.
+            """
+    }
+]
