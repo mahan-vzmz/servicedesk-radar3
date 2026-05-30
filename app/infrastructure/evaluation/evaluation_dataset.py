1559ecaa8e56073db4e53aad935a1ef4aa45e89f
mahan-vzmz <mahan.varmazyar@gmail.com>
2026-05-31 02:42:08 +0330

    Update evaluation_dataset.py

--- app/infrastructure/evaluation/evaluation_dataset.py
+++ app/infrastructure/evaluation/evaluation_dataset.py
@@ -1,42 +1,35 @@
 EVALUATION_DATASET = [
 
     {
-        "query":
-            "VPN MFA issue",
-
-        "relevant_ids":
-            [1, 2, 3]
+        "query": "VPN MFA issue",
+        "relevant_ids": [1, 2, 3, 4] 
     },
-
     {
-        "query":
-            "VPN login failed",
-
-        "relevant_ids":
-            [1, 2, 3]
+        "query": "VPN login failed",
+        "relevant_ids": [1, 2, 3, 4]
     },
-
     {
-        "query":
-            "cannot connect to VPN",
-
-        "relevant_ids":
-            [1, 2, 3]
+        "query": "cannot connect to VPN",
+        "relevant_ids": [1, 2, 3, 4]
     },
-
     {
-        "query":
-            "Outlook login problem",
-
-        "relevant_ids":
-            [5]
+        "query": "Outlook login problem",
+        "relevant_ids": [5, 6, 7]  
     },
-
     {
-        "query":
-            "printer offline",
-
-        "relevant_ids":
-            [4]
-    }
+        "query": "email not working",
+        "relevant_ids": [5, 6, 7]
+    },
+    {
+        "query": "printer offline",
+        "relevant_ids": [8, 9] 
+    },
+    {
+        "query": "no internet access",
+        "relevant_ids": [10, 11]
+    },
+    {
+        "query": "account locked",
+        "relevant_ids": [12, 13]
+    },
 ]
